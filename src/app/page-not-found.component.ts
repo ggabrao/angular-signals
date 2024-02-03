f4ab99c7e661f7e328c969ecdf2dc65627148c57
Gabriel <gguimaraesabrao@gmail.com>
2024-02-03 14:38:49 -0300

    defining initial structure

---
+++ src/app/page-not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [],
+  template: ''
+})
+export class PageNotFoundComponent {
+
+}
