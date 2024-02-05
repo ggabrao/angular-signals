d9de11182584bebb8eed7957c1a762f6c848ed02
Gabriel <gguimaraesabrao@gmail.com>
2024-02-05 07:17:05 -0300

    setting up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { IntroComponent } from './intro.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'intro', component: IntroComponent },
+    {
+        path: 'courses',
+        loadComponent: () => import('./courses/courses-list.component').then(c => c.CoursesListComponent)
+    },
+    {
+        path: 'cart',
+        loadComponent: () => import('./cart/shell.component').then(c => c.ShellComponent)
+    },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
